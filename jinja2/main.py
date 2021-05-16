daf836cadd6f15a74fedab4fef6a601af738c59c
SVA <sva1.0@mail.ru>
2021-05-16 16:23:46 +0700

    Шаблон настроен на содержимое файла с продукцией

---
+++ jinja2/main.py
@@ -0,0 +1,51 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+caps = [
+    {
+        "title": "Красная кепка",
+        "price": "$ 100.00",
+        "image": ""
+    },
+    {
+        "title": "Чёрная кепка",
+        "price": "$ 120.00",
+        "image": "https://dvmn.org/filer/canonical/1569333143/330/"
+    },
+    {
+        "title": "Ещё одна чёрная кепка",
+        "price": "$ 90.00",
+        "image": "https://dvmn.org/filer/canonical/1569333144/331/"
+    },
+    {
+        "title": "Камуфляжная кепка",
+        "price": "$ 100.00",
+        "image": "https://dvmn.org/filer/canonical/1569943835/350/"
+    },
+    {
+        "title": "Джинсовая кепка",
+        "price": "$ 100.00",
+        "image": "https://dvmn.org/filer/canonical/1569943835/351/"
+    },
+    {
+        "title": "Плоская кепка",
+        "price": "$ 100.00",
+        "image": "https://dvmn.org/filer/canonical/1569943836/352/"
+    }
+]
+
+rendered_page = template.render(caps=caps)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8001), SimpleHTTPRequestHandler)
+server.serve_forever()
--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
+import pprint
+import collections
+import pandas as pd
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+df_products = pd.read_excel('./data/wine2.xlsx', keep_default_na=False, na_values=None)
+products = {x: [] for x in df_products['Категория'].unique()} # products = collections.defaultdict(list)
+
+for product in range(df_products.shape[0]):
+    products[df_products['Категория'][product]].append(dict(df_products.iloc[product, ]))
+
+pp = pprint.PrettyPrinter()
+pp.pprint(products)
+
+rendered_page = template.render(
+    manufactory_age=datetime.datetime.now().year-1920,
+    products=products,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
