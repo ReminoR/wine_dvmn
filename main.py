c49c38641b382137f21995511e62f5b01e81d187
Victor Shamakov <sva1.0@mail.ru>
2021-05-20 11:05:12 +0700

    В файле скрипта main.py используется if __name__ == '__main__'. Что позволяет без опасений использовать его, как модуль.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
-import pprint
 import collections
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -19,13 +18,18 @@ for product in range(df_products.shape[0]):
     products[df_products['Категория'][product]].append(dict(df_products.iloc[product, ]))
 
 
-rendered_page = template.render(
-    manufactory_age=datetime.datetime.now().year-1920,
-    products=products,
-)
+def main():
+    rendered_page = template.render(
+        manufactory_age=datetime.datetime.now().year-1920,
+        products=products,
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
