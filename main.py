beebb68c459b07798fe2a31127be6158af24e19a
SVA <sva1.0@mail.ru>
2021-05-23 13:12:40 +0700

    Добавлены аргументы командной строки с помощью argparse. Удалены отладочные файлы

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import argparse
 import datetime
 import collections
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filepath', default='./data/catalog.xlsx')
 
-catalog = pd.read_excel('./data/catalog.xlsx', keep_default_na=False, na_values=None).to_dict(orient='records')
-products = collections.defaultdict(list)
-
-for product in catalog:
-    products[product['Категория']].append(product)
+    return parser
 
 
-def main():
+def open_catalog(file):
+    catalog = pd.read_excel(file, keep_default_na=False, na_values=None).to_dict(orient='records')
+    products = collections.defaultdict(list)
+
+    for product in catalog:
+        products[product['Категория']].append(product)
+
+    return products
+
+
+def create_template():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
     rendered_page = template.render(
         manufactory_age=datetime.datetime.now().year-1920,
         products=products,
@@ -27,8 +39,21 @@ def main():
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
+
+def run_server():
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
+
+
+def main():
+    parser = createParser()
+    namespace = parser.parse_args()
+
+    print(namespace)
+
+    products = open_catalog(namespace.filepath)
+    create_template()
+    run_server()
 
 
 if __name__ == '__main__':
